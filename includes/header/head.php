838f9bccbef0fe32c038f85573ee59030386de23
mwaura <petermwaurawanjohi7@gmail.com>
2019-07-17 14:37:32 +0300

    header

--- includes/header/head.php
+++ includes/header/head.php
@@ -1,8 +1,8 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<link href="/Resources/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="/Resources/fa/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<script src="/Resources/js/jquery-3.4.0.min.js" type="text/javascript"></script>
+<script src="/Resources/js/popper.min.js" type="text/javascript"></script>
+<script src="/Resources/js/bootstrap.min.js" type="text/javascript"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Emerans Systems Ltd</title>
