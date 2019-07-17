1f48bea4767d3c311567724f9b06c2683990f68d
Kevo Kario <kevokario@gmail.com>
2019-07-17 14:46:32 +0300

    Neet code

--- index.php
+++ index.php
@@ -6,20 +6,46 @@ and open the template in the editor.
 -->
 <html>
     <head>
-
-       <?php
-       include './includes/header/head.php';
-       ?>
+        <!--Head details Section-->
+        <?php
+        include './includes/header/head.php';
+        ?>
     </head>
     <body>
+
+        <!--Nav Bar Section-->
         <?php
-       include './includes/header/navbar.php';
-       include './home/carousel.php';
-       include './home/expertise.php';
-       include './home/electrification.php';
-       include './home/contactus.php';
-       include './home/projects.php';
-       include './includes/footer/footer.php';
-       ?>
+        include './includes/header/navbar.php';
+        ?>
+
+        <!--Carousel Section-->
+        <?php
+        include './home/carousel.php';
+        ?>
+
+        <!--Expertise Section-->
+        <?php
+        include './home/expertise.php';
+        ?>
+
+        <!--Electrification Section-->
+        <?php
+        include './home/electrification.php';
+        ?>
+
+        <!--contact us section-->
+        <?php
+        include './home/contactus.php';
+        ?>
+
+        <!--Projects Section-->
+        <?php
+        include './home/projects.php';
+        ?>
+
+        <!--Footer Section-->
+        <?php
+        include './includes/footer/footer.php';
+        ?>
     </body>
 </html>
