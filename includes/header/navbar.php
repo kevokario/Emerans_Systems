e9a225e599d96e61393f7f171f68837d5e5fed5a
Kevo Kario <kevokario@gmail.com>
2019-07-17 15:34:51 +0300

    wip

--- includes/header/navbar.php
+++ includes/header/navbar.php
@@ -1,8 +1,23 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a href="#" class="navbar-brand">
+        <i class="fa fa-home"></i>
+    </a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fa fa-user-o"></i> Link Item 1
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fa fa-user-o"></i> 
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
