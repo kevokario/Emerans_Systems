c33387cd0badd5133834c6aa9225d7dc4e2f197d
mwaura <petermwaurawanjohi7@gmail.com>
2019-07-17 17:38:22 +0300

    footer

--- includes/footer/footer.php
+++ includes/footer/footer.php
@@ -1,8 +1,74 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="container-fluid bg-dark" id="footer">
+    <div class="row">
+        <div class="col-sm-3 bg-warning">
+            <h5>EMERAND SYSTEMS LTD</h5>
+            <span>
+            <h6>FOLLOW US!</h6>
+                <a class="fa fa-facebook"></a>
+                <a class="fa fa-twitter">
+                <a class="fa fa-linkedin"></a>
+                <a class="fa fa-youtube"></a>
+                </a></span>
+        </div>
+        <div class="col-sm-4">
+            <h6 class="text-center">SERVICES/EXPERTISE</h6>
+            <div class="row">
+            <div class="col-sm-6">
+            <p style="font-weight:900;">DESIGN AND SURVEY</p>
+            <p>Field survey<br>
+                Autocard design<br>
+                Field database<br>
+                Field database<br>
+            Costing</p>
+            <p style="font-weight:900;">LV and MV CONSTRUCTION</p>
+            <p>
+                Pegging<br>
+                Material requisition<br>
+                Construction standards<br>
+                Metering
+            </p></div>
+            <div class="col-sm-6">
+            <h8>PROJECT MANAGEMENT</h8>
+            <p>
+                Electrification System<br>
+                Supervision team<br>
+                Back office
+            </p>
+            <p style="font-weight:900;">STAFFING and TRAINING<p>
+                Constraction teams<br>
+                Surveyers and designers<br>
+                Autocard<br>
+                Supervision<br>
+                Back Office
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <h5>ELECTRIFICATION SYSTEM </h5>
+            <div class="container">
+                <p><a onmouseover="style='color: white;'" href="#">Design</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Store Management</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Work Progress</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Reporting</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Gant chart</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Material reconciliation</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Returns</a><br>
+                    <a onmouseover="style='color: white;'" href="#">Inspection and handover</a>
+            </p>
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <a href="#"><h6>PROJECTS</h6></a>
+            <a href="#"><h6>BLOG</h6>
+                <a href="#"><H6>ABOUT US</H6></a>
+                <div class="container">
+                    <a onmouseover="style='color: white;'" href="#"><P>Our <br>company<br></a>
+                        <a onmouseover="style='color: white;'" href="#">Our Offices</a> <br>
+                        <a onmouseover="style='color: white;'" href="#">Our team</a> <br>
+                <a onmouseover="style='color: white;'" href="#">Contact Us</a><br>
+                <a onmouseover="style='color: white;'" href="#">Careers</a></p>
+                </div>
+        </div>
+    </div>
+</div>
 
