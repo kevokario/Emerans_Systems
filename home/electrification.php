266e6bcfadfc44cccf83e27cf0a359b368898e8d
mwaura <petermwaurawanjohi7@gmail.com>
2019-07-19 12:15:28 +0300

    contact

--- home/electrification.php
+++ home/electrification.php
@@ -3,31 +3,31 @@
         <div class="col-sm-3 bg-warning" id="electrifictaion">
             <h5>ELECTRIFICATION SYSTEMS </h5>
             <div class="container">
-            <p>Our systems accept excel data input<br> i.e schemes allocated,<br> indicating on the map exact location of the schemes.
-                <br> This helps in planning
-                logistics of material transportation to the site,<br> the route to use.....</p>
+                <p>Our systems accept excel data input<br> i.e schemes allocated,<br> indicating on the map exact location of the schemes.
+                    <br> This helps in planning
+                    logistics of material transportation to the site,<br> the route to use.....</p>
             </div>
         </div>
-        <div class="col-sm-9">
-          <div id="carousel-elec" class="carousel slide  carousel-multi-item v-2" data-ride='carousel'>
+<!--        <div class="col-sm-9">
+            <div id="carousel-elec" class="carousel slide  carousel-multi-item v-2" data-ride='carousel'>
                 <ul class="carousel-indicators">
-                <li data-target="#carousel-elec" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-elec" data-slide-to="1"></li>
-                <li data-target="#carousel-elec" data-slide-to="2"></li>
-            </ul>
+                    <li data-target="#carousel-elec" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-elec" data-slide-to="1"></li>
+                    <li data-target="#carousel-elec" data-slide-to="2"></li>
+                </ul>
                 <div class="carousel-inner" id="carspan">
                     <div class="carousel-item active">
-                            <div class="row">
-        <div class="col-sm-4 zoom1" id="training">
-            <img src="img/training3.jpeg.jpg" width="150px" alt="training"/>
-        </div>
-        <div class="col-sm-4 zoom1">
-            <img src="img/Training.jpg" width="150px" alt=""/>
-        </div>
-        <div class="col-sm-4 zoom1">
-            <img src="img/Emeran_open_page.png" width="150px" alt=""/>
-        </div>
-    </div>
+                        <div class="row">
+                            <div class="col-sm-4 zoom1" id="training">
+                                <img src="img/training3.jpeg.jpg" width="150px" alt="training"/>
+                            </div>
+                            <div class="col-sm-4 zoom1">
+                                <img src="img/Training.jpg" width="150px" alt=""/>
+                            </div>
+                            <div class="col-sm-4 zoom1">
+                                <img src="img/Emeran_open_page.png" width="150px" alt=""/>
+                            </div>
+                        </div>
                         <div class="carousel-caption">
                             <h3>Electrifiction</h3>
                             <p>we are the best in electric connections</p>
@@ -40,17 +40,17 @@
                         </a>
                     </div>
                     <div class="carousel-item">
-                            <div class="row">
-        <div class="col-sm-4 zoom1" id="training">
-            <img src="img/training3.jpeg.jpg" alt="training"/>
-        </div>
-        <div class="col-sm-4 zoom1">
-            <img src="img/Training.jpg" alt=""/>
-        </div>
-        <div class="col-sm-4 zoom1">
-            <img src="img/Emeran_open_page.png" alt=""/>
-        </div>
-    </div>
+                        <div class="row">
+                            <div class="col-sm-4 zoom1" id="training">
+                                <img src="img/training3.jpeg.jpg" alt="training"/>
+                            </div>
+                            <div class="col-sm-4 zoom1">
+                                <img src="img/Training.jpg" alt=""/>
+                            </div>
+                            <div class="col-sm-4 zoom1">
+                                <img src="img/Emeran_open_page.png" alt=""/>
+                            </div>
+                        </div>
                         <a class="carousel-control-prev" href="#carousel-elec" data-slide="prev">
                             <span class="carousel-control-prev-icon"></span>
                         </a>
@@ -59,17 +59,17 @@
                         </a>
                     </div>
                     <div class="carousel-item">
-                            <div class="row">
-        <div class="col-sm-4 zoom1" id="training">
-            <img src="img/training3.jpeg.jpg" alt="training"/>
-        </div>
-        <div class="col-sm-4 zoom1">
-            <img src="img/Training.jpg" alt=""/>
-        </div>
-        <div class="col-sm-4 zoom1">
-            <img src="img/Emeran_open_page.png" alt=""/>
-        </div>
-    </div>
+                        <div class="row">
+                            <div class="col-sm-4 zoom1" id="training">
+                                <img src="img/training3.jpeg.jpg" alt="training"/>
+                            </div>
+                            <div class="col-sm-4 zoom1">
+                                <img src="img/Training.jpg" alt=""/>
+                            </div>
+                            <div class="col-sm-4 zoom1">
+                                <img src="img/Emeran_open_page.png" alt=""/>
+                            </div>
+                        </div>
                         <div class="carousel-caption">
                             <h3>Electrifiction</h3>
                             <p>we are the best in electric connections</p>
@@ -83,7 +83,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>-->
     </div>
-    
+
 </div>
--- home/projects.php
+++ home/projects.php
@@ -1,8 +1,22 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="container" id="projects">
+    <h3>PROJECTS</h3>
+    <p>Emeran systems helps companies of all sizes, from 
+        different industries succeed in their engineering objectives.<br>
+        Check out some of our latest engineering projects! 
+    </p>
+    <div class="row">
+        <div class="col-sm-3">
+            <p>Design and salary</p>
+        </div>
+        <div class="col-sm-3">
+            <p>Electrification management systems</p>
+        </div>
+        <div class="col-sm-3">
+            <p>Training</p>
+        </div>
+        <div class="col-sm-3">
+            <p>Electrification management systems phase II</p>
+        </div>
+    </div>
+</div>
 
