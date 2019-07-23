d1722010eea22b2b18aadb8c0ae98a1f49d2a2e7
mwaura <petermwaurawanjohi7@gmail.com>
2019-07-23 10:50:09 +0300

    linking

--- Expertise/head.php
+++ Expertise/head.php
@@ -76,39 +76,37 @@
 			</div>
 		</li>
 		<li>
-                    <a href="Electrification systems/" id="linksB" class="mynav-link">
+                    <a href="../Electrification_Systems" id="linksB" class="mynav-link">
 				Electrification Systems
 				<i class="mynav-caret pull-right fa fa-caret-down"></i>
 			</a>
 			<div class="menu-options custom-menu-option text-dark">
 				<div class="menu-option-body">
-						<a>sublink a</a>
-						<a>sublink b</a>
-						<a>sublink c</a>
-						<a>sublink d</a>
-						<a>sublink e</a>
-						<a>sublink f</a>
-						<a>sublink g</a>
-						<a>sublink h</a>
+						<a>Design</a>
+						<a>Store management</a>
+						<a>Work progress</a>
+						<a>Reporting</a>
+						<a>Gantt chart</a>
+						<a>Material reconciliation</a>
 					</div>
 			</div>
 		</li>
 		<li>
-                    <a href="projects/" id="linksC" class="mynav-link">
+                    <a href="../projects/" id="linksC" class="mynav-link">
 				Projects 
 				<i class="mynav-caret pull-right  fa fa-caret-down"></i>
 			</a>
 			<div class="menu-options custom-menu-option text-dark">
 				<div class="menu-option-body">
-						<a>sublink a</a>
-						<a>sublink b</a>
-						<a>sublink c</a>
-						<a>sublink d</a>
+						<a>Design & Salary</a>
+						<a>Electrification Management Systems</a>
+						<a>Training</a>
+						<a>Consultance</a>
 					</div>
 			</div>
 		</li>
 		<li>
-			<a href="#" id="linksD" class="mynav-link">
+			<a href="" id="linksD" class="mynav-link">
 				Blog
 				<!-- <i class="mynav-caret pull-right fa fa-caret-down"></i> -->
 			</a>
@@ -122,7 +120,7 @@
 			</div> -->
 		</li>
 		<li>
-			<a href="#" id="linksE" class="mynav-link">
+			<a href="" id="linksE" class="mynav-link">
 				Contact us 
 				<!-- <i class="mynav-caret pull-right fa fa-caret-down"></i> -->
 			</a>
