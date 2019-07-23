33f59bc0f9ce08d87a317e454c2a6fb258093f55
mwaura <petermwaurawanjohi7@gmail.com>
2019-07-23 15:38:37 +0300

    electrification

--- pages/Electrificationsystemsmain.php
+++ pages/Electrificationsystemsmain.php
@@ -1,37 +1,28 @@
 <div class="container-fluid generic-container custom-option-header">
     <div class="row">
         <div class="col-sm-12">
-            <h2></h2>
-            <p></p>
+            <h2> Electrification systems Section</h2>
+             <p class="emphase-introduction">
+                World-class engineering services at your fingertips
+            </p>
         </div>
     </div>
 </div>
 <div class="cotainer-fluid generic-container">
-    <div class="row">
-        <div class="col-sm-12 text-center">
-            <h2> Electrification systems Section</h2>
-
-            <p class="emphase-introduction">
-                World-class engineering services at your fingertips
-            </p>
-
-        </div>
-    </div>
     <div class="row px-auto  justify-content-center">
 
         <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
-            <img src="img/training3.jpeg.jpg" alt="training" class="service-img"/>
+            <img src="../img/1.jpg" alt="training" class="service-img"/>
             <div class="service-title">
-                <h3>Design and Survey</h3>
+                <h3>Design and Budgeting</h3>
             </div>
             <div class="service-cover">
                 <div class="service-cover-title">
-                    <h3>Design and Survey</h3>
+                    <h3>Design and Budgeting</h3>
                 </div>
                 <div class="service-cover-text">
-                    I contain some content<br>
-                    I contain some content<br>
-                    I contain some content
+                    <p>Our systems accept excel data input i.e schemes allocated, indicating on the map that’s
+                        (integrated on the system), exact location of the schemes.</p>
                 </div>
                 <div class="service-cover-link">
                     Learn more <i class="fa fa-long-arrow-right"></i>
@@ -39,18 +30,17 @@
             </div>
         </a>
         <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
-            <img src="img/Training.jpg" alt="training" class="service-img"/>
+            <img src="../img/store.jpg" alt="store" class="service-img"/>
             <div class="service-title">
-                <h3>I am A title</h3>
+                <h3>Store Management</h3>
             </div>
             <div class="service-cover">
                 <div class="service-cover-title">
-                    <h3>I am a title</h3>
+                    <h3>Store Management</h3>
                 </div>
                 <div class="service-cover-text">
-                    I contain some content<br>
-                    I contain some content<br>
-                    I contain some content
+                   <p>In the store module which has user permissions to store manager and procurement officers.
+                       The system indicates the inventory level for all the materials i.e low, critically low, high, etc.</p>
                 </div>
                 <div class="service-cover-link">
                     Learn more <i class="fa fa-long-arrow-right"></i>
