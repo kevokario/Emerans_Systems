257128611ca5f8c0789edb786c86c5fc8ef77db9
Kevo Kario <kevokario@gmail.com>
2019-07-23 13:30:42 +0300

    complete modofy of project section

--- home/projects.php
+++ home/projects.php
@@ -1,68 +1,76 @@
-<!--<div id="projects">
-    <h3>PROJECTS</h3>
-    <p>Emeran systems helps companies of all sizes, from 
-        different industries succeed in their engineering objectives.<br>
-        Check out some of our latest engineering projects! 
-    </p>
+<div class="container-fluid generic-container-alt mb-5">
     <div class="row">
-        <a class="col-sm-3 projectimg" href="#">
+        <div class="col-sm-12">
+            <h2 class="custom-heading">PROJECTS</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-7">
+            <p>Emeran systems helps companies of all sizes, from 
+                different industries succeed in their engineering objectives.
+                Check out some of our latest engineering projects! 
+            </p>
+        </div>
+        <div class="col-sm-5">
+            <a href="#" class="my-class">
+                View all projects +
+            </a>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+
+        <a class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xm-12 custom-link" href="#">
             <div class="card">
                 <div class="card-title">
-                <img src="img/pb1.jpg" alt=""/>
+                    <img src="img/pb1.jpg" class="card-img" alt=""/>
                 </div>
                 <div class="card-body">
                     <p>Design and salary</p>
-                    <p id="learnmore">LEARN MORE
+                    <p class="custom-card-link">LEARN MORE
                         <span href="#" class="fa fa-long-arrow-right fa-lg"></span>
                     </p>
                 </div>
             </div>
         </a>
-        <a class="col-sm-3 projectimg" href="#">
+        <a class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xm-12 custom-link" href="#">
             <div class="card">
                 <div class="card-title">
-                <img src="img/pbanner.jpg" alt=""/>
+                    <img src="img/pbanner.jpg" class="card-img" alt=""/>
                 </div>
                 <div class="card-body">
                     <p>Electrification management systems</p>
-                    <p id="learnmore">LEARN MORE
+                    <p class="custom-card-link" >LEARN MORE
                         <span href="#" class="fa fa-long-arrow-right fa-lg"></span>
                     </p>
                 </div>
             </div>
         </a>
-        <a class="col-sm-3 projectimg" href="#">
+        <a class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xm-12 custom-link" href="#">
             <div class="card">
                 <div class="card-title">
-            <img src="img/photo_2017-10-20_12-52-49.jpg"  alt=""/>
+                    <img src="img/photo_2017-10-20_12-52-49.jpg" class="card-img" alt=""/>
                 </div>
-            <div class="card-body">
-            <p>Training</p>
-            <p id="learnmore">LEARN MORE
-                <span href="#" class="fa fa-long-arrow-right fa-lg"></span>
-            </p>
-            </div>
+                <div class="card-body">
+                    <p>Training</p>
+                    <p class="custom-card-link">LEARN MORE
+                        <span href="#" class="fa fa-long-arrow-right fa-lg"></span>
+                    </p>
+                </div>
             </div>
         </a>
-        <a class="col-sm-3 projectimg" href="#">
+        <a class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xm-12 custom-link" href="#">
             <div class="card">
                 <div class="card-title">
-                    <img src="img/photo_2019-06-03_17-14-58.jpg"  alt=""/>
+                    <img src="img/photo_2019-06-03_17-14-58.jpg" class="card-img" alt=""/>
                 </div>
-            <div class="card-body">
-            <p>Electrification management systems phase II</p>
-            <p id="learnmore">LEARN MORE<span href="#" class="fa fa-long-arrow-right fa-lg"></span>
-            </p>
-            </div>
+                <div class="card-body">
+                    <p>Electrification management systems phase II</p>
+                    <p class="custom-card-link">LEARN MORE
+                        <span href="#" class="fa fa-long-arrow-right fa-lg"></span>
+                    </p>
+                </div>
             </div>
         </a>
-    </div>
-</div>
--->
-<div class="container-fluid generic-container-alt">
-    <div class="row">
-        <div class="col-sm-12">
-            <h2>PROJECTS</h2>
-        </div>
     </div>
 </div>
