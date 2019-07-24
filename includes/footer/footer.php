9ed14db18dcb55e481b16568d3af622107414406
Kevo Kario <kevokario@gmail.com>
2019-07-24 10:15:22 +0300

    modify footer section

--- includes/footer/footer.php
+++ includes/footer/footer.php
@@ -3,47 +3,57 @@
         <div class="col-sm-3  emebottom" id="emebottom">
             <h5>EMERAND SYSTEMS LTD</h5>
             <div id="emeran">
-                <p style="color: white;">
+                <p class='text-white'>
                     Emeran systems is a kenyan registered company operating in the Engineering and Technology field. The company focus on 
                     i) Power distribution. i.e distribution networks, Electrification, low voltage and medium voltage design & Constrution.
 
                     Weve qualified surveyor, Designers, electrical linesmen, electricians, with extensive experience in line design, survey, management and construction.
                 </p>
             </div>
-            <span>
-                <h6>FOLLOW US!</h6>
-                <a class="fa fa-facebook fa-2x" href="#"></a>
-                <a class="fa fa-twitter fa-2x" href="#"></a>
-                <a class="fa fa-linkedin fa-2x" href="#"></a>
-                <a class="fa fa-youtube fa-2x" href="#"></a>
-            </span>
+            <div>
+                <h6 class="text-center">FOLLOW US!</h6>
+                <div class="d-flex justify-content-center">
+                    <a class="p-2 fa fa-facebook fa-2x" href="#"></a>
+                    <a class="p-2 fa fa-twitter fa-2x" href="#"></a>
+                    <a class="p-2 fa fa-linkedin fa-2x" href="#"></a>
+                    <a class="p-2 fa fa-youtube fa-2x" href="#"></a>
+                </div>
+            </div>
         </div>
         <div class="col-sm-4 foot">
             <h6 class="text-center">SERVICES/EXPERTISE</h6>
             <div class="row">
                 <div class="col-sm-6">
-                    <p style="font-weight:900;">DESIGN AND SURVEY</p>
-                    <p id="hide">Field survey<br>
+                    <h6>DESIGN AND SURVEY</h6>
+                    <p class="hide">Field survey<br>
                         Autocard design<br>
                         Field database<br>
                         Field database<br>
-                        Costing</p>
-                    <p style="font-weight:900;">LV and MV CONSTRUCTION</p>
-                    <p id="hide">
+                        Costing
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <h6>LV and MV CONSTRUCTION</h6>
+                    <p class="hide">
                         Pegging<br>
                         Material requisition<br>
                         Construction standards<br>
                         Metering
-                    </p></div>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-sm-6">
-                    <h8>PROJECT MANAGEMENT</h8>
-                    <p id="hide">
+                    <h6>PROJECT MANAGEMENT</h6>
+                    <p class="hide">
                         Electrification System<br>
                         Supervision team<br>
                         Back office
                     </p>
-                    <p style="font-weight:900;">STAFFING and TRAINING</p>
-                    <p id="hide">   Constraction teams<br>
+                </div>
+                <div class="col-sm-6">
+                    <h6>STAFFING & TRAINING</h6>
+                    <p class="hide">   Constraction teams<br>
                         Surveyers and designers<br>
                         Autocard<br>
                         Supervision<br>
@@ -54,15 +64,15 @@
         </div>
         <div class="col-sm-3 foot">
             <h5>ELECTRIFICATION SYSTEMS </h5>
-            <div class="container" id="hide">
-                <p><a onmouseover="style = 'color: white;'" href="#">Design</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Store Management</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Work Progress</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Reporting</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Gant chart</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Material reconciliation</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Returns</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Inspection and handover</a>
+            <div class="container" class="hide">
+                <p><a href="#">Design</a><br>
+                    <a  href="#">Store Management</a><br>
+                    <a  href="#">Work Progress</a><br>
+                    <a  href="#">Reporting</a><br>
+                    <a  href="#">Gant chart</a><br>
+                    <a  href="#">Material reconciliation</a><br>
+                    <a  href="#">Returns</a><br>
+                    <a  href="#">Inspection and handover</a>
                 </p>
             </div>
         </div>
@@ -70,12 +80,12 @@
             <a href="#"><h6>PROJECTS</h6></a>
             <a href="#"><h6>BLOG</h6>
                 <a href="#"><H6>ABOUT US</H6></a>
-                <div class="container" id="hide">
-                    <a onmouseover="style = 'color: white;'" href="#"><P>Our <br>company<br></a>
-                    <a onmouseover="style = 'color: white;'" href="#">Our Offices</a> <br>
-                    <a onmouseover="style = 'color: white;'" href="#">Our team</a> <br>
-                    <a onmouseover="style = 'color: white;'" href="#">Contact Us</a><br>
-                    <a onmouseover="style = 'color: white;'" href="#">Careers</a></p>
+                <div class="container" class="hide">
+                    <a  href="#"><P>Our <br>company<br></a>
+                    <a  href="#">Our Offices</a> <br>
+                    <a  href="#">Our team</a> <br>
+                    <a  href="#">Contact Us</a><br>
+                    <a  href="#">Careers</a></p>
                 </div>
         </div>
     </div>
