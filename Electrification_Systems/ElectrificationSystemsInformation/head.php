bebd0c48b75cacec67608f62b0b7bf17020104df
mwaura <petermwaurawanjohi7@gmail.com>
2019-07-24 09:14:20 +0300

    electrification information

---
+++ Electrification_Systems/ElectrificationSystemsInformation/head.php
@@ -0,0 +1,124 @@
+<head>
+<link href="../../Resources/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="../../Resources/fa/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<link href="../../custom/css/style1.css" rel="stylesheet" type="text/css"/>
+<link href="../../custom/css/customcss.css" rel="stylesheet" type="text/css"/>
+<link href="../../Resources/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+<link href="../../Resources/css/animate.css" rel="stylesheet" type="text/css"/>
+<link href="../../custom/css/style2.css" rel="stylesheet" type="text/css"/>
+<link rel="icon" href="../../img/favicon.png"/>
+<script src="../../Resources/js/jquery-3.4.0.min.js" type="text/javascript"></script>
+<script src="../../Resources/js/popper.min.js" type="text/javascript"></script>
+<script src="../../Resources/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../../custom/js/custom.js" type="text/javascript"></script>
+<script src="../../Resources/js/swiper.min.js" type="text/javascript"></script>
+<script src="../../Resources/js/jquery.wait.js" type="text/javascript"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Emerans Systems Ltd</title>
+</head>
+<body>
+    <nav class="mynav navbar-light">
+        <a class="mynav-brand" href="../"><img src="../img/logo.png" alt="Emerans" class="mynav-logo"></a>
+	<ul class="mynav-links">
+		<li>
+                    <a id="linksA" href="../Expertise/" class="mynav-link">
+				Expertise 
+				<i class="mynav-caret pull-right fa fa-caret-down"></i>
+			</a>
+			<div class="menu-options-multiple custom-menu-option text-dark">
+				<div class="mymenu">
+                                    <a class="menu-option-title" href="../Expertise/Design & Survey/">
+						Design & Survey
+					</a>
+					<div class="menu-option-body">
+                                            <a href="../Expertise/Design & Survey/AutocardDesign.php">Autocard Design</a>
+                                            <a href="../Expertise/Design & Survey/Costing.php">Costing</a>
+                                            <a href="../Expertise/Design & Survey/Design&Survey.php">Design&Survey</a>
+                                            <a href="../Expertise/Design & Survey/Field Survey.php">Field Survey</a>
+                                            <a href="../Expertise/Design & Survey/FieldDatabase.php">Field Database</a>
+					</div>
+				</div>
+				<div class="mymenu">
+					<div class="menu-option-title">
+					Lv & Mv Construction
+					</div>
+					<div class="menu-option-body">
+						<a>Construction Standards</a>
+						<a>Material Requisition</a>
+						<a>Metering</a>
+						<a>Pegging</a>
+					</div>
+				</div>
+				<div class="mymenu">
+					<div class="menu-option-title">
+						Project Management
+					</div>
+					<div class="menu-option-body">
+						<a>Back Office</a>
+						<a>Electrification System</a>
+						<a>Project Management</a>
+						<a>Supervision Team</a>
+					</div>
+				</div>
+				<div class="mymenu">
+					<div class="menu-option-title">
+						Staffing and Training
+					</div>
+					<div class="menu-option-body">
+						<a>Auto Card</a>
+						<a>Back Office</a>
+						<a>Construction Teams</a>
+						<a>Staffing and Training</a>
+                                                <a>Supervisors</a>
+                                                <a>Surveyors and Designers</a>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li>
+                    <a href="" id="linksB" class="mynav-link">
+				Electrification Systems
+				<i class="mynav-caret pull-right fa fa-caret-down"></i>
+			</a>
+			<div class="menu-options custom-menu-option text-dark">
+				<div class="menu-option-body">
+						<a>Design</a>
+						<a>Store management</a>
+						<a>Work progress</a>
+						<a>Reporting</a>
+						<a>Gantt chart</a>
+						<a>Material reconciliation</a>
+					</div>
+			</div>
+		</li>
+		<li>
+                    <a href="../projects/" id="linksC" class="mynav-link">
+				Projects 
+				<i class="mynav-caret pull-right  fa fa-caret-down"></i>
+			</a>
+			<div class="menu-options custom-menu-option text-dark">
+				<div class="menu-option-body">
+						<a>Design & Salary</a>
+						<a>Electrification Management Systems</a>
+						<a>Training</a>
+						<a>Consultance</a>
+					</div>
+			</div>
+		</li>
+		<li>
+			<a href="#" id="linksD" class="mynav-link">
+				Blog
+				<!-- <i class="mynav-caret pull-right fa fa-caret-down"></i> -->
+			</a>
+		</li>
+		<li>
+			<a href="#" id="linksE" class="mynav-link">
+				Contact us 
+				<!-- <i class="mynav-caret pull-right fa fa-caret-down"></i> -->
+			</a>
+		</li>
+	</ul>
+	<button class="mynav-btn btn pull-right">
+		<i class="fa fa-reorder"></i>
+	</button>
+</nav>
