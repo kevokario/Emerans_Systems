c09bf3872a64237c5eeed566d9fd1cf615d06244
Kevo Kario <kevokario@gmail.com>
2019-07-24 16:21:00 +0300

    work in progress

--- Expertise/Project Management/index.php
+++ Expertise/Project Management/index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-    
-   <?php
-   include 'head.php';
-   ?>
-        <div style="height: 600px">
-            
-        </div>
 
-        <!--Footer Section-->
-        <?php
-        include '../../includes/footer/Footer1.php';
-        ?>
-    </body>
+    <?php
+    include 'head.php';
+    ?>
+    <!--Content Section-->
+    <?php
+    include '../../pages/ProjectManagementMain.php';
+    ?>
+    <!--Footer Section-->
+    <?php
+    include '../../includes/footer/Footer1.php';
+    ?>
+</body>
 </html>
---
+++ pages/ProjectManagementMain.php
@@ -0,0 +1,111 @@
+<div class="container-fluid generic-container custom-option-header">
+    <div class="row">
+        <div class="col-sm-12">
+            <h2> Project Management</h2>
+            <p class="emphase-introduction">
+               Creaform’s product development and industrial design services give you the professionals, 
+               technology and expertise you need to create innovative product designs that provide
+               the right quality to the 
+               right market—all while mitigating unnecessary costs and risks.
+            </p>
+        </div>
+    </div>
+</div>
+<div class="cotainer-fluid generic-container">
+    <div class="row px-auto  justify-content-center">
+
+     
+        <a href="BackOffice.php" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
+            <img src="../../img/store.jpg" alt="store" class="service-img"/>
+            <div class="service-title">
+                <h3>Back Office</h3>
+            </div>
+            <div class="service-cover">
+                <div class="service-cover-title">
+                    <h3>Back Office</h3>
+                </div>
+                <div class="service-cover-text">
+                    <p>In the store module which has user permissions to store manager and procurement officers.
+                        The system indicates the inventory level for all the materials i.e low, critically low, high, etc.</p>
+                </div>
+                <div class="service-cover-link">
+                    Learn more <i class="fa fa-long-arrow-right"></i>
+                </div>
+            </div>
+        </a>
+        <a href="ElectrificationSystem.php" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
+            <img src="../../img/cumulative_work_progress.png" alt="training" class="service-img"/>
+            <div class="service-title">
+                <h3>Electrification Systems</h3>
+            </div>
+            <div class="service-cover">
+                <div class="service-cover-title">
+                    <h3>Electrification Systems</h3>
+                </div>
+                <div class="service-cover-text">
+                    <p>After the allocation of the scheme to a subcontractor or construction teams, the system
+                        gives a platform for them to update their work progress,</p>
+                </div>
+                <div class="service-cover-link">
+                    Learn more <i class="fa fa-long-arrow-right"></i>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="row px-auto  justify-content-center">
+        
+        <a href="ProjectManagement.php" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
+            <img src="../../img/training3.jpeg.jpg" alt="training" class="service-img"/>
+            <div class="service-title">
+                <h3>Project Management</h3>
+            </div>
+            <div class="service-cover">
+                <div class="service-cover-title">
+                    <h3>Project Management</h3>
+                </div>
+                <div class="service-cover-text">
+                    <p>In reporting the system captures both the individual and overall project progress, it has a
+                        chart indicating the overall progress accumulative work completed, and accumulative work
+                        in progress.</p>
+                </div>
+                <div class="service-cover-link">
+                    Learn more <i class="fa fa-long-arrow-right"></i>
+                </div>
+            </div>
+        </a>
+
+        <a href="SupervisionTeam.php" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
+            <img src="../../img/Gantt_chart_(2).png" alt="training" class="service-img"/>
+            <div class="service-title">
+                <h3>Supervision Management</h3>
+            </div>
+            <div class="service-cover">
+                <div class="service-cover-title">
+                    <h3>Supervision Management</h3>
+                </div>
+                <div class="service-cover-text">
+                    <p>A Gantt chart is a chart in which a series of horizontal lines shows the amount of work done,
+r production completed in a certain period of time in relation to the amount planned for
+those periods.</p>
+                </div>
+                <div class="service-cover-link">
+                    Learn more <i class="fa fa-long-arrow-right"></i>
+                </div>
+            </div>
+        </a>
+        
+    </div>
+</div>
+<div class="container-fluid generic-container container-contact">
+    <div class="row fancy-background">
+        <div class="col-sm-8">
+            <h2>LAERN MORE ABOUT HOW OUR<br> PROJECT MANAGEMENT</h2>
+            <p>Emeran system's team can provide the experience, expertise, resourses and materials you need.</p>
+        </div>
+        <div class="col-sm-4">
+            <a href="#" class="btn fancy-button">
+                Contact Us
+            </a>
+        </div>
+    </div>
+</div>
