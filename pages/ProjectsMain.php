c1b8586ba2fcd0a9f06a8f7331b4ab62b6533fff
Kevo Kario <kevokario@gmail.com>
2019-07-25 09:21:45 +0300

    work in progress

--- pages/ProjectsMain.php
+++ pages/ProjectsMain.php
@@ -1,22 +1,13 @@
 <div class="container-fluid generic-container custom-option-header">
     <div class="row">
         <div class="col-sm-12">
-            <h2></h2>
-            <p></p>
+            <h2> Projects Section</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
 </div>
 <div class="cotainer-fluid generic-container">
-    <div class="row">
-        <div class="col-sm-12 text-center">
-            <h2> Projects Section</h2>
-
-            <p class="emphase-introduction">
-                World-class engineering services at your fingertips
-            </p>
-
-        </div>
-    </div>
     <div class="row px-auto  justify-content-center">
 
         <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
@@ -39,13 +30,13 @@
             </div>
         </a>
         <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
-            <img src="img/Training.jpg" alt="training" class="service-img"/>
+            <img src="../img/Training.jpg" alt="training" class="service-img"/>
             <div class="service-title">
-                <h3>I am A title</h3>
+                <h3>Electrification Management Systems</h3>
             </div>
             <div class="service-cover">
                 <div class="service-cover-title">
-                    <h3>I am a title</h3>
+                     <h3>Electrification Management Sys</h3>
                 </div>
                 <div class="service-cover-text">
                     I contain some content<br>
@@ -57,14 +48,16 @@
                 </div>
             </div>
         </a>
+    </div>
+    <div class="row justify-content-center px-auto">
         <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
-            <img src="img/Emeran_open_page.png" alt="training" class="service-img"/>
+            <img src="../img/Emeran_open_page.png" alt="training" class="service-img"/>
             <div class="service-title">
-                <h3>I am A title</h3>
+                <h3>Training</h3>
             </div>
             <div class="service-cover">
                 <div class="service-cover-title">
-                    <h3>I am a title</h3>
+                    <h3>Training</h3>
                 </div>
                 <div class="service-cover-text">
                     I contain some content<br>
@@ -77,13 +70,13 @@
             </div>
         </a>
         <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
-            <img src="img/Emeran_open_page.png" alt="training" class="service-img"/>
+            <img src="../img/Emeran_open_page.png" alt="training" class="service-img"/>
             <div class="service-title">
-                <h3>I am A title</h3>
+                <h3>Consultants</h3>
             </div>
             <div class="service-cover">
                 <div class="service-cover-title">
-                    <h3>I am a title</h3>
+                    <h3>Consultants</h3>
                 </div>
                 <div class="service-cover-text">
                     I contain some content<br>
@@ -98,27 +91,13 @@
     </div>
     <div class="row px-auto  justify-content-center">
 
-        <a href="#" class="service-container  col-lg-3 col-md-4 col-sm-6 col-xm-12">
-            <img src="img/training3.jpeg.jpg" alt="training" class="service-img"/>
-            <div class="service-title">
-                <h3>Design and Survey</h3>
-            </div>
-            <div class="service-cover">
-                <div class="service-cover-title">
-                    <h3>Design and Survey</h3>
-                </div>
-                <div class="service-cover-text">
-                    I contain some content<br>
-                    I contain some content<br>
-                    I contain some content
-                </div>
-                <div class="service-cover-link">
-                    Learn more <i class="fa fa-long-arrow-right"></i>
-                </div>
-            </div>
-        </a>
     </div>
 </div>
+<style>
+    .service-container:nth-child(4){
+        display: block;
+    }
+</style>
 <div class="container-fluid generic-container container-contact">
     <div class="row fancy-background">
         <div class="col-sm-8">
