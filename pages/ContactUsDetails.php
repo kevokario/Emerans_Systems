c516bb26de94af81ce7c4daed76a249677bfeafd
Kevo Kario <kevokario@gmail.com>
2019-07-25 09:42:31 +0300

    build contuct us template

--- pages/ContactUsDetails.php
+++ pages/ContactUsDetails.php
@@ -1,2 +1,25 @@
-
+<div class="container-fluid generic-container-alt">
+    
+    <!--This is the title area-->
+    <div class="row">
+        <div class="col-sm-12">
+            <h2 class="custom-heading">
+                Contact us details
+            </h2>
+        </div>
+    </div>
+    
+    <!--This is the details Section-->
+    <div class="row">
+        <!--Details Area-->
+        <div class="col-sm-6">
+            
+        </div>
+        <!--Map Area-->
+        <div class="col-sm-6">
+            
+        </div>
+    </div>
+    
+</div>
 
--- pages/ContactUsForm.php
+++ pages/ContactUsForm.php
@@ -1,1 +1,31 @@
-
+<div class="container-fluid generic-container">
+    Here we have the title and some paragraph
+    <div class="row">
+        <div class="col-sm-12">
+            <h2 class="custom-heading">Contact us</h2>
+            <p>
+                <!--Place content Here-->
+            </p>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-sm-12">
+            
+            <!--Here we place the form-->
+            <form>
+                <div class="col-sm-6">
+                    <!--First details here-->
+                </div>
+                
+                <div class="col-sm-6">
+                    <!--Test area section-->
+                </div>
+                
+                <div class="col-sm-12">
+                    <!--Button Section-->
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
